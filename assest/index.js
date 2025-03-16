1d3afbacc56b91b18e57c2ee14601e4b85f30335
IqbalMauld <140826458+IqbalMauld@users.noreply.github.com>
2025-03-16 17:08:13 +0700

    Update index.js

--- assest/index.js
+++ assest/index.js
@@ -1,3 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navItems = document.querySelectorAll(".navbar-nav a");
+  
+    navItems.forEach((item, index) => {
+        gsap.to(item, {
+            opacity: 1,
+            x: 0,
+            duration: 1,
+            delay: index * 0.8, // Stagger the animation
+            ease: "power2.out"
+        });
+    });
+  });
+
+  gsap.registerPlugin(TextPlugin, ScrollTrigger);
+  
+  gsap.from('.cron', {duration: 1.5, delay: 1.5, x: -100, opacity: 0});
+  gsap.from('.lett', {duration: 1.5, delay: 1.5, x: 100, opacity: 0});
+  gsap.to(".lead", { duration: 2 , delay: 2.5, text : 'Ga cuma bikin website, tapi juga buat design keren'});
+  gsap.from('#cta', {duration: 3, delay: 3, opacity: 0});
+  
+
+
+
 const navbarNav = document.querySelector('.navbar-nav')
 const menu = document.getElementById('menu')
 
